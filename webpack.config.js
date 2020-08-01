1737758114cbf1259ba9a9d3d2e980728cba7bc2
Dariy <gomzykovad@gmail.com>
2020-08-01 20:35:51 +1000

    Разбила остальной код на модули

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     filename: `bundle.js`,
     path: path.join(__dirname, `public`),
   },
-  devtool: `soutce-map`,
+  devtool: `source-map`,
   devServer: {
     contentBase: path.join(__dirname, `public`),
     watchContentBase: true,
